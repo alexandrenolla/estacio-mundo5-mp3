806d6a6264e4851ccbd7b18a94dc223531aa32dd
Alexandre Henrique Nolla <alexandre.nolla@gmail.com>
2024-11-01 22:48:55 -0300

    Update index.py

--- index.py
+++ index.py
@@ -2,20 +2,17 @@ import pandas as pd
 import numpy as np
 
 data = pd.read_csv('data.csv', sep=';', engine='python', encoding='utf-8')
-data_copy = data.copy()
-# Substitui os valores nulos em 'Calories' por zero
-data_copy['Calories'] = data_copy['Calories'].fillna(0)
+copy = data.copy()
 
-# Substitui os valores nulos em 'Date' por '1900/01/01'
-data_copy['Date'] = data_copy['Date'].fillna('1900/01/01')
+copy['Calories'] = copy['Calories'].fillna(0)
 
-# Transforma os dados de 'Date' em datetime
-data_copy['Date'] = data_copy['Date'].str.replace("'", "", regex=False)
-data_copy['Date'] = data_copy['Date'].str.replace('20201226', '2020/12/26')
-data_copy['Date'] = data_copy['Date'].replace('1900/01/01', np.nan)
-data_copy['Date'] = pd.to_datetime(data_copy['Date'], format='%Y/%m/%d', errors='coerce')
+copy['Date'] = copy['Date'].fillna('1900/01/01')
 
-# Remove registros contendo valores nulos
-data_copy = data_copy.dropna()
+copy['Date'] = copy['Date'].str.replace("'", "", regex=False)
+copy['Date'] = copy['Date'].str.replace('20201226', '2020/12/26')
+copy['Date'] = copy['Date'].replace('1900/01/01', np.nan)
+copy['Date'] = pd.to_datetime(data_copy['Date'], format='%Y/%m/%d', errors='coerce')
 
-print(data_copy)
+copy = copy.dropna()
+
+print(copy)
